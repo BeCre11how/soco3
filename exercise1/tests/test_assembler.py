10e211fc5c9f6d18699ba079f569067459cc2214
B1s9l <furrerbasil@gmail.com>
2023-11-26 15:13:25 +0100

    Simplified and formated existing code

--- exercise1/tests/test_assembler.py
+++ exercise1/tests/test_assembler.py
@@ -1,16 +1,28 @@
 import subprocess
 import os
 
+# Support function
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines()
+
+# Assembler Path
+assembler_path = "../vm/assembler.py"
+
+# Temporary Output Path
+output_path = "AS_test_output_temp.mx"
+
 ################################################################################################################
 #ASSEMBLER TEST 1
 ################################################################################################################
 
 def test_assembler_program1():
+    # Program Name
+    program = "program1"
+    
     # Path to the assembler script
-    assembler_path = "../vm/assembler.py"
-    input_path = "as_files_ok/program1.as"
-    output_path = "AS_test_output_temp.mx"
-    expected_output_path = "mx_files_ok/program1.mx"
+    input_path = "as_files_ok/" + program + ".as"
+    expected_output_path = "mx_files_ok/" + program + ".mx"
     
     # Run the assembler on program1.as
     subprocess.run(["python", assembler_path, input_path, output_path])
@@ -18,72 +30,50 @@ def test_assembler_program1():
     # Ensure the ASSEMBLER output file exists
     assert os.path.exists(output_path), "ASSEMBLER did not create the output file"
 
-    # Read the output from the Assembler
-    with open(output_path, "r") as file:
-        assembler_output = file.read()
-        os.remove(output_path)
-
-    # Read the expected output
-    with open(expected_output_path, "r") as file:
-        expected_output = file.read()
-
     # Assert that the assembler's output matches the expected output
-    assert assembler_output == expected_output, "Assembler output does not match expected output for program1.as"
+    assert read_file(output_path) == read_file(expected_output_path), "Assembler output does not match expected output for " + program + ".as"
+    os.remove(output_path)
 
 ################################################################################################################
 #ASSEMBLER TEST 2
 ################################################################################################################
 
 def test_assembler_program2():
+    # Program Name
+    program = "program2"
+    
     # Path to the assembler script
-    assembler_path = "../vm/assembler.py"
-    program_path = "as_files_ok/program2.as"
-    output_path = "AS_test_output_temp.mx"
-    expected_output_path = "mx_files_ok/program2.mx"
+    input_path = "as_files_ok/" + program + ".as"
+    expected_output_path = "mx_files_ok/" + program + ".mx"
     
     # Run the assembler on program1.as
-    subprocess.run(["python", assembler_path, program_path, output_path])
+    subprocess.run(["python", assembler_path, input_path, output_path])
     
     # Ensure the ASSEMBLER output file exists
     assert os.path.exists(output_path), "ASSEMBLER did not create the output file"
 
-    # Read the output from the Assembler
-    with open(output_path, "r") as file:
-        assembler_output = file.read()
-        os.remove(output_path)
-
-    # Read the expected output
-    with open(expected_output_path, "r") as file:
-        expected_output = file.read()
-
     # Assert that the assembler's output matches the expected output
-    assert assembler_output == expected_output, "Assembler output does not match expected output for program1.as"
+    assert read_file(output_path) == read_file(expected_output_path), "Assembler output does not match expected output for " + program + ".as"
+    os.remove(output_path)
 
 ################################################################################################################
 #ASSEMBLER TEST 3
 ################################################################################################################
 
 def test_assembler_program3():
+    # Program Name
+    program = "program3"
+    
     # Path to the assembler script
-    assembler_path = "../vm/assembler.py"
-    program_path = "as_files_ok/program3.as"
-    output_path = "AS_test_output_temp.mx"
-    expected_output_path = "mx_files_ok/program3.mx"
+    input_path = "as_files_ok/" + program + ".as"
+    expected_output_path = "mx_files_ok/" + program + ".mx"
     
     # Run the assembler on program1.as
-    subprocess.run(["python", assembler_path, program_path, output_path])
-
+    subprocess.run(["python", assembler_path, input_path, output_path])
+    
     # Ensure the ASSEMBLER output file exists
     assert os.path.exists(output_path), "ASSEMBLER did not create the output file"
 
-    # Read the output from the Assembler
-    with open(output_path, "r") as file:
-        assembler_output = file.read()
-        os.remove(output_path)
-
-    # Read the expected output
-    with open(expected_output_path, "r") as file:
-        expected_output = file.read()
-
     # Assert that the assembler's output matches the expected output
-    assert assembler_output == expected_output, "Assembler output does not match expected output for program1.as"
+    assert read_file(output_path) == read_file(expected_output_path), "Assembler output does not match expected output for " + program + ".as"
+    os.remove(output_path)
--- exercise1/tests/test_vm.py
+++ exercise1/tests/test_vm.py
@@ -1,35 +1,38 @@
 import subprocess
 import os
-import pytest
+
+# Support function
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines()
+        
+# VM path
+vm_path = "../vm/vm.py"
+
+# Temporary Output Path
+output_path = "vm_test_output_temp.txt"
 
 ################################################################################################################
 #VM TEST 1
 ################################################################################################################
 
 def test_vm_program1():
+    # Program Name
+    program = "program1"
+     
     # Path to the VM script, for the .mx file, the expected output file and for the VM output file
-    vm_path = "../vm/vm.py"
-    input_path = "mx_files_ok/program1.mx"
-    output_path = "vm_test_output_temp.txt"
-    expected_output_path = "vm_outputs_ok/output1.txt"
+    input_path = "mx_files_ok/" + program + ".mx"
+    expected_output_path = "vm_outputs_ok/" + program + ".txt"
     
-    # Run the VM on program1.mx
+    # Run the VM on the program
     subprocess.run(["python", vm_path, input_path, output_path])
 
     # Ensure the VM output file exists
     assert os.path.exists(output_path), "VM did not create the output file"
 
-    # Read the output from the VM
-    with open(output_path, "r") as file:
-        vm_output = file.read()
-        os.remove(output_path)
-
-    # Read the expected output
-    with open(expected_output_path, "r") as file:
-        expected_output = file.read()
-
     # Assert that the VM's output matches the expected output
-    assert vm_output == expected_output, "VM output does not match expected output for program1.mx"
+    assert read_file(output_path) == read_file(expected_output_path), "VM output does not match expected output for " + program + ".mx"
+    os.remove(output_path)
 
 
 ################################################################################################################
@@ -37,69 +40,57 @@ def test_vm_program1():
 ################################################################################################################
 
 def test_vm_program2():
-    # Path to the VM script, for the .mx file, the expected output file and for the VM output file
-    vm_path = "../vm/vm.py"
-    input_path = "mx_files_ok/program2.mx"
-    output_path = "vm_test_output_temp.txt"
-    expected_output_path = "vm_outputs_ok/output2.txt"
+    # Program Name
+    program = "program2"
     
-    # Run the VM on program1.mx
+    # Path to the VM script, for the .mx file, the expected output file and for the VM output file
+    input_path = "mx_files_ok/" + program + ".mx"
+    expected_output_path = "vm_outputs_ok/" + program + ".txt"
+    
+    # Run the VM on the program
     subprocess.run(["python", vm_path, input_path, output_path])
 
     # Ensure the VM output file exists
     assert os.path.exists(output_path), "VM did not create the output file"
 
-    # Read the output from the VM
-    with open(output_path, "r") as file:
-        vm_output = file.read()
-        os.remove(output_path)
-
-    # Read the expected output
-    with open(expected_output_path, "r") as file:
-        expected_output = file.read()
-
     # Assert that the VM's output matches the expected output
-    assert vm_output == expected_output, "VM output does not match expected output for program1.mx"
+    assert read_file(output_path) == read_file(expected_output_path), "VM output does not match expected output for " + program + ".mx"
+    os.remove(output_path)
 
 ################################################################################################################
 #VM TEST 3
 ################################################################################################################
 
 def test_vm_program3():
-    # Path to the VM script, for the .mx file, the expected output file and for the VM output file
-    vm_path = "../vm/vm.py"
-    input_path = "mx_files_ok/program3.mx"
-    output_path = "vm_test_output_temp.txt"
-    expected_output_path = "vm_outputs_ok/output3.txt"
+    # Program Name
+    program = "program3"
     
-    # Run the VM on program1.mx
+    # Path to the VM script, for the .mx file, the expected output file and for the VM output file
+    input_path = "mx_files_ok/" + program + ".mx"
+    expected_output_path = "vm_outputs_ok/" + program + ".txt"
+    
+    # Run the VM on the program
     subprocess.run(["python", vm_path, input_path, output_path])
 
     # Ensure the VM output file exists
     assert os.path.exists(output_path), "VM did not create the output file"
 
-    # Read the output from the VM
-    with open(output_path, "r") as file:
-        vm_output = file.read()
-        os.remove(output_path)
-
-    # Read the expected output
-    with open(expected_output_path, "r") as file:
-        expected_output = file.read()
-
     # Assert that the VM's output matches the expected output
-    assert vm_output == expected_output, "VM output does not match expected output for program1.mx"
+    assert read_file(output_path) == read_file(expected_output_path), "VM output does not match expected output for " + program + ".mx"
+    os.remove(output_path)
     
 ################################################################################################################
 #OUT OF MEMORY TEST [DONE]
 ################################################################################################################
 
 def test_vm_out_of_memory():
+    # Program Name
+    program = "out_of_memory"
+    
     # Path to the VM script
-    vm_path = "../vm/vm.py"
-    input_path = "mx_files_ok/out_of_memory.mx"
-    output_path = "vm_test_output_temp.txt"
+    input_path = "mx_files_ok/" + program + ".mx"
 
+    # Run the VM on the program and save error messages
     result = subprocess.run(["python", vm_path, input_path, output_path], stderr=subprocess.PIPE, text=True)
 
     assert result.returncode != 0, "VM should have exited with an error"
@@ -112,11 +103,13 @@ def test_vm_out_of_memory():
 ################################################################################################################
 
 def test_vm_instruction_not_found():
+    # Program Name
+    program = "invalid_instruction"
+    
     # Path to the VM script
-    vm_path = "../vm/vm.py"
-    input_path = "mx_files_ok/invalid_instruction.mx"
-    output_path = "vm_test_output_temp.txt"
+    input_path = "mx_files_ok/" + program + ".mx"
 
+    # Run the VM on the program and save error messages
     result = subprocess.run(["python", vm_path, input_path, output_path], stderr=subprocess.PIPE, text=True)
 
     assert result.returncode != 0, "VM should have exited with an error"
