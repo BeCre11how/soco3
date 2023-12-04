be8681a7ad4835183e91d5670552dd9906490eb8
BeCre11how <gianandreadaniel.gerber@uzh.ch>
2023-12-04 18:19:14 +0100

    final changes fixed breakpoints bug

--- debugger/vm_break.py
+++ debugger/vm_break.py
@@ -56,11 +56,21 @@ class VirtualMachineBreak(VirtualMachineExtend):
     def _do_add_breakpoint(self, addr):
         if self.ram[addr] == OPS["brk"]["code"]:
             return True
+        assert addr >= 0 and addr < len(self.ram), f"{addr} out of range"
+        assert self.__lastPopulatedSmaller(addr), f"cannot set breakpoint outside of programm"
         self.breaks[addr] = self.ram[addr]
         self.ram[addr] = OPS["brk"]["code"]
         return True
     # [/add]
 
+
+    def __lastPopulatedSmaller(self, addr):
+        count = 0
+        for i in range(len(self.ram)):
+            if self.ram[i] == 1:
+                break
+            count += 1
+        return True if addr <= count else False
     # [clear]
     def _do_clear_breakpoint(self, addr):
         if self.ram[addr] != OPS["brk"]["code"]:
@@ -71,14 +81,11 @@ class VirtualMachineBreak(VirtualMachineExtend):
     # [/clear]
 
     def _do_add_watchpoint(self, addr):
-        assert addr < len(self.ram), f"addr not valid in memory nor registers\n"
+        assert addr < len(self.ram) and addr >= -1, f"addr not valid in memory nor registers\n"
 
         if addr < len(self.ram):
             val = self.ram[addr]
-            self.watchpoints.append([addr, val, True])
-        if addr < len(self.reg):
-            val = self.reg[addr]
-            self.watchpoints.append([addr, val, False])
+            self.watchpoints.append([addr, val])
         return True
 
     def _do_erase_watchpoint(self, addr):
@@ -90,15 +97,12 @@ class VirtualMachineBreak(VirtualMachineExtend):
 
     def _update_watchpoints(self):
         for x in self.watchpoints:
-            x[1] = self.ram[x[0]] if x[2] else self.reg[x[0]]
+            x[1] = self.ram[x[0]]
 
     def _check_watchpoints(self):
         for x in self.watchpoints:
-            if x[1] != self.ram[x[0]] and x[2]:
+            if x[1] != self.ram[x[0]]:
                 self.write(f"old: {x[1]} new: {self.ram[x[0]]}")
-                return True
-            if x[0] < NUM_REG and x[1] != self.reg[x[0]] and not x[2]:
-                self.write(f"old: {x[1]} new: {self.reg[x[0]]}")
                 return True
         return False
 
